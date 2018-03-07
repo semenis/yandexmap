2e498c99ee309f2de9faddd4d1227469234cd3a9
Семён <semen.say@ya.ru>
2018-03-07 18:34:51 +0300

    update

--- main.py
+++ main.py
@@ -5,6 +5,90 @@ import os
 
 spn = 25
 
+###GUI
+class Label:
+    def __init__(self, rect, text):
+        self.rect = pygame.Rect(rect)
+        self.text = text
+        self.bgcolor = pygame.Color("white")
+        self.font_color = pygame.Color("gray")
+        # Рассчитываем размер шрифта в зависимости от высоты
+        self.font = pygame.font.Font(None, self.rect.height - 4)
+        self.rendered_text = None
+        self.rendered_rect = None
+
+
+    def render(self, surface):
+        surface.fill(self.bgcolor, self.rect)
+        self.rendered_text = self.font.render(self.text, 1, self.font_color)
+        self.rendered_rect = self.rendered_text.get_rect(x=self.rect.x + 2, centery=self.rect.centery)
+        # выводим текст
+        surface.blit(self.rendered_text, self.rendered_rect)
+
+class GUI:
+    def __init__(self):
+        self.elements = []
+
+    def add_element(self, element):
+        self.elements.append(element)
+
+    def render(self, surface):
+        for element in self.elements:
+            render = getattr(element, "render", None)
+            if callable(render):
+                element.render(surface)
+
+    def update(self):
+        for element in self.elements:
+            update = getattr(element, "update", None)
+            if callable(update):
+                element.update()
+
+    def get_event(self, event):
+        for element in self.elements:
+            get_event = getattr(element, "get_event", None)
+            if callable(get_event):
+                element.get_event(event)
+
+
+class Button(Label):
+    def __init__(self, rect, text):
+        super().__init__(rect, text)
+        self.bgcolor = pygame.Color("blue")
+        # при создании кнопка не нажата
+        self.pressed = False
+
+    def render(self, surface):
+        surface.fill(self.bgcolor, self.rect)
+        self.rendered_text = self.font.render(self.text, 1, self.font_color)
+        if not self.pressed:
+            color1 = pygame.Color("white")
+            color2 = pygame.Color("black")
+            self.rendered_rect = self.rendered_text.get_rect(x=self.rect.x + 5, centery=self.rect.centery)
+        else:
+            color1 = pygame.Color("black")
+            color2 = pygame.Color("white")
+            self.rendered_rect = self.rendered_text.get_rect(x=self.rect.x + 7, centery=self.rect.centery + 2)
+
+        # рисуем границу
+        pygame.draw.rect(surface, color1, self.rect, 2)
+        pygame.draw.line(surface, color2, (self.rect.right - 1, self.rect.top), (self.rect.right - 1, self.rect.bottom), 2)
+        pygame.draw.line(surface, color2, (self.rect.left, self.rect.bottom - 1),
+                         (self.rect.right, self.rect.bottom - 1), 2)
+        # выводим текст
+        surface.blit(self.rendered_text, self.rendered_rect)
+
+    def get_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.pressed = self.rect.collidepoint(event.pos)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            self.pressed = False
+
+
+####
+
+
+
 response = None
 try:
     map_request = "http://static-maps.yandex.ru/1.x/?ll=133.795384,-25.694768&spn={0},{0}&l=sat".format(str(spn))
@@ -31,22 +115,32 @@ except IOError as ex:
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
+
+gui = GUI()
+sloy = ('map','sat','skl')
+b1 = Button((10, 65, 150, 80), sloy[0])
+
+
 # Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
+gui.add_element(b1)
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-import random
+
 running = True
-vistrels = []
+
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        gui.get_event(event);
     pygame.time.wait(10)
     screen.blit(pygame.image.load(map_file), (0, 0))
-
+    # отрисовываем все GUI-элементы
+    gui.render(screen)
+    # обновляеем все GUI-элементы
+    gui.update()
 
 
     pygame.display.flip()
